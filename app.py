18c2b03c174422f5f8405481ba9c515f14097983
Bhudevi-Vattikuti <102209138+Bhudevi-Vattikuti@users.noreply.github.com>
2022-03-23 13:02:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.title("SPAM HAM CLASSIFIER")
 text_model=joblib.load('spam-ham')
 ip=st.text_input("Enter your message: ")
 op=text_model.predict([ip])
+
 if st.button('Predict'):
   st.title(op[0])
   
